a208787bdda2f0f092744e732891e6c96a47599f
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-05-13 12:56:46 +0200

    Changed theme to catppuccin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,13 +7,14 @@ return {
       dark_variant = "moon",
       styles = {
         bold = true,
-        italic = true,
+        italic = false,
         transparency = true,
       },
       -- disable_background = false,
       highlight_groups = {
         CurSearch = { fg = "base", bg = "leaf", inherit = false },
         Search = { fg = "text", bg = "leaf", blend = 20, inherit = false },
+        Comment = { italic = true },
       },
       groups = {
         border = "muted",
@@ -47,10 +48,10 @@ return {
       },
     },
     -- COMMENT TO ACTIVATE
-    config = function(_, opts)
-      require("rose-pine").setup(opts)
-      vim.cmd("colorscheme rose-pine") -- optional
-    end,
+    -- config = function(_, opts)
+    --   require("rose-pine").setup(opts)
+    --   vim.cmd("colorscheme rose-pine") -- optional
+    -- end,
   },
   {
     "folke/tokyonight.nvim",
@@ -113,6 +114,71 @@ return {
       --         end
       --     end,
       -- })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.05, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = function(colors)
+          return {
+            NormalFloat = { bg = colors.base, fg = colors.text },
+          }
+        end,
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          snacks = true,
+          harpoon = true,
+          fzf = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
